e5ec24a94dc2061eab277b92abfca78aa9997801
facundoenrique <facuenrique@gmail.com>
2020-10-29 23:53:01 -0300

    Update test.c

--- test.c
+++ test.c
@@ -1,34 +1,7 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
 #include "mapeo.h"
 #include "lista.h"
-
-
-
-/**
-    Funcion para comparar dos claves, las claves en este caso son caracteres.
-    El comparador retorna 0 si las dos claves son IGUALES.
-**/
-
-int comparacion_claves_evaluador(char* palabra1, char* palabra2)
-{
-
-    int i = 0;
-    while ((*((palabra1+i))!='\0') && (*((palabra2+i))!='\0') && (*(palabra1+i)==*(palabra2+i)))
-          i=i+1;
-    if (*(palabra1+i)!='\0' || *(palabra2+i)!='\0')
-       return 1;
-    else return 0;
-
-}
-
-
-
-//Una ayudante paso una pagina con codigos de hash y copie este
-/**
-    Funcion hash que convierte los caracteres en un entero.
-**/
 
 int funcion_hash_evaluador(char *palabra)
 {
@@ -37,78 +10,204 @@ int funcion_hash_evaluador(char *palabra)
 
     while (c = *palabra++)
         hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-
     return hash;
 }
 
-void ingresarDatos(tMapeo map){
-
-    printf("#Ingrese palabras al mapeo#\n");
-    char *palabra;
-    palabra=(char*)malloc(40*sizeof(char));
-
-
-    tLista *l=(tLista*)malloc(sizeof(tLista)*10);
-    crear_lista(&*l);
-
-    for (int i = 0;i<5;i++){
-        printf("Ingrese una palabra \n");
-        scanf("%s",palabra);
-
-        int *valor;
-        valor=(int*)malloc(sizeof(int));
-        *valor=(int)m_recuperar(map,palabra);
-        struct entrada *nueva_entrada=(struct entrada*)malloc(sizeof(struct entrada));
-
-        nueva_entrada->clave=(funcion_hash_evaluador(palabra)%5);
-        nueva_entrada->valor=valor;
-
-        l_insertar(*l,l_primera(*l),nueva_entrada);
-        if (*valor==NULL){
-            printf("La palabra no se encuentra en el texto. \n");
-            m_insertar(map,palabra,valor);
-        }
-        else
-        {
-            if (valor>1)
-                printf("La palabra %s se encuentra %d veces en el texto. \n",palabra,valor);
-            else
-                printf("La palabra %s se encuentra 1 vez en el texto. \n",palabra);
-        }
-
-        int h=0;
-        while (h<40)  //borro palabra usada
-        {
-                 palabra[h]=0;
-
-                        h++;
-             }
-    }
-
-    tPosicion paRecorrer;
-    paRecorrer=(tPosicion)l_primera(*l);
-    printf("voy a eliminar \n");
-    for (int j=0 ; j<5 ; j++){
-
-        tEntrada elem = (tEntrada)l_recuperar(*l,paRecorrer);
-        tClave c = (int*)elem->clave;
-        tValor v = (int*)elem->valor;
-
-  //      tValor v = (tValor) m_recuperar(map,elem->clave); //tengo que mostrar que tengo adentro del mapeo
-//        printf("clave en mapeo: %s",v);
-        printf("clave-valor en lista: %s | %i \n",c,v);
-        paRecorrer = (tEntrada)l_siguiente(*l,paRecorrer);
-    }
-    printf("\nElimine todo ");
-}
-
-
-int main(int argc, char *argv[])    //--------- agregue un * mas
+/**
+    Funcion que elimina la clave que le pasan como parametro,
+    en este caso se trata de caracteres.
+**/
+void funcion_eliminar_claves_evaluador(char *palabra)
 {
-    printf("##### TESTEANDO A MANO #####\n");
-    tMapeo mapeo;
-    crear_mapeo(&mapeo,10,&funcion_hash_evaluador,&comparacion_claves_evaluador);
-    ingresarDatos(mapeo);
-
-    return 0;
+    free(palabra);
 }
+
+/**
+    Funcion que elimina el valor que le pasan como parametro,
+    en este caso se trata de un entero.
+**/
+void funcion_eliminar_valores_evaluador(int *valor)
+{
+    free(valor);
+}
+
+int comparacion_claves_evaluador(char* palabra1, char* palabra2)
+{
+
+    int i = 0;
+    if (strlen(palabra1)==strlen(palabra2)){
+        while ((*((palabra1+i))!='\0') && (*((palabra2+i))!='\0') && (*(palabra1+i)==*(palabra2+i)))
+              i=i+1;
+        if (*(palabra1+i)!='\0' && *(palabra2+i)!='\0')
+        {
+            ;return 1;
+        }
+
+        else {
+                    ;return 0;
+        }
+    }else {
+
+        return 1;
+    }
+
+
+}
+
+
+
+int main()
+{
+	tMapeo map;
+    crear_mapeo(&map,5,&funcion_hash_evaluador,&comparacion_claves_evaluador);
+    char *palabra;
+    palabra=(char*)malloc(sizeof(char));
+
+    int *valor;
+    int *valor_recuperado;
+
+    valor=(int*)malloc(sizeof(int));
+    valor_recuperado=(int*)malloc(sizeof(int));
+
+    printf("Palabra 1: \n");
+    scanf("%s",palabra);
+        int hash = funcion_hash_evaluador(palabra)%(map->longitud_tabla);
+
+    *valor_recuperado=m_recuperar(map,(tClave)palabra);
+    printf("valor_recuperado antes %d | %i\n",*valor_recuperado,hash);
+    *valor=m_insertar(map,(tClave)palabra,(*valor_recuperado+1));
+    *valor_recuperado=m_recuperar(map,(tClave)palabra);
+    if (valor==NULL)
+        printf("El valor era nulo\n");
+    else
+        printf("el valor era: %d | el valor_recuperado es: %d\n \n",*valor,*valor_recuperado);
+
+    printf("palabra: %s\n",palabra);
+
+
+
+     palabra=NULL;
+    palabra=(char*)malloc(sizeof(char));
+    printf("Palabra 2: \n");
+    scanf("%s",palabra);
+    hash = funcion_hash_evaluador(palabra)%(map->longitud_tabla);
+    *valor_recuperado=m_recuperar(map,(tClave)palabra);
+    printf("valor_recuperado antes %d | %i \n",*valor_recuperado,hash);
+    *valor=m_insertar(map,(tClave)palabra,(*valor_recuperado+1));
+    *valor_recuperado=m_recuperar(map,(tClave)palabra);
+    if (valor==NULL)
+        printf("El valor era nulo\n");
+    else
+        printf("el valor era: %d | el valor_recuperado es: %d\n \n",*valor,*valor_recuperado);
+
+
+
+    palabra=NULL;
+    palabra=(char*)malloc(sizeof(char));
+    printf("Palabra 3: \n");
+    scanf("%s",palabra);
+    hash = funcion_hash_evaluador(palabra)%(map->longitud_tabla);
+    *valor_recuperado=m_recuperar(map,(tClave)palabra);
+    printf("valor_recuperado antes %d | %i \n",*valor_recuperado,hash);
+    *valor=m_insertar(map,palabra,(*valor_recuperado+1));
+    *valor_recuperado=m_recuperar(map,palabra);
+    if (valor==NULL)
+        printf("El valor era nulo\n");
+    else
+        printf("el valor era: %d | el valor_recuperado es: %d\n \n",*valor,*valor_recuperado);
+
+
+
+    palabra=NULL;
+    palabra=(char*)malloc(sizeof(char));
+    printf("Palabra 4: \n");
+    scanf("%s",palabra);
+    hash = funcion_hash_evaluador(palabra)%(map->longitud_tabla);
+
+    *valor_recuperado=m_recuperar(map,(tClave)palabra);
+    printf("valor_recuperado antes %d | %i \n",*valor_recuperado,hash);
+    *valor=m_insertar(map,palabra,(*valor_recuperado+1));
+    *valor_recuperado=m_recuperar(map,palabra);
+    if (valor==NULL)
+        printf("El valor era nulo\n");
+    else
+        printf("el valor era: %d | el valor_recuperado es: %d\n \n",*valor,*valor_recuperado);
+
+
+/*
+
+    palabra=NULL;
+    palabra=(char*)malloc(sizeof(char));
+    printf("Palabra 5: \n");
+    scanf("%s",palabra);
+    *valor_recuperado=m_recuperar(map,palabra);
+    printf("valor_recuperado antes %d | \n",*valor_recuperado);
+    *valor=m_insertar(map,palabra,(*valor_recuperado+1));
+    *valor_recuperado=m_recuperar(map,palabra);
+    if (valor==NULL)
+        printf("El valor era nulo\n");
+    else
+        printf("el valor era: %d | el valor_recuperado es: %d\n \n",*valor,*valor_recuperado);
+
+
+
+    palabra=NULL;
+    palabra=(char*)malloc(sizeof(char));
+    printf("Palabra 6: \n");
+    scanf("%s",palabra);
+    *valor_recuperado=m_recuperar(map,palabra);
+    printf("valor_recuperado antes %d | \n",*valor_recuperado);
+    *valor=m_insertar(map,palabra,(*valor_recuperado+1));
+    *valor_recuperado=m_recuperar(map,palabra);
+    if (valor==NULL)
+        printf("El valor era nulo\n");
+    else
+        printf("el valor era: %d | el valor_recuperado es: %d\n \n",*valor,*valor_recuperado);
+
+
+    palabra=NULL;
+    palabra=(char*)malloc(sizeof(char));
+    printf("Palabra 7: \n");
+    scanf("%s",palabra);
+    *valor_recuperado=m_recuperar(map,palabra);
+    printf("valor_recuperado antes %d | \n",*valor_recuperado);
+    *valor=m_insertar(map,palabra,(*valor_recuperado+1));
+    *valor_recuperado=m_recuperar(map,palabra);
+    if (valor==NULL)
+        printf("El valor era nulo\n");
+    else
+        printf("el valor era: %d | el valor_recuperado es: %d\n \n",*valor,*valor_recuperado);
+
+*/
+
+/*
+    palabra=NULL;
+    palabra=(char*)malloc(sizeof(char));
+    printf("Palabra a eliminar: \n");
+    scanf("%s",palabra);
+    m_eliminar(map,palabra,&funcion_eliminar_claves_evaluador,&funcion_eliminar_valores_evaluador);
+
+
+    palabra=NULL;
+    palabra=(char*)malloc(sizeof(char));
+    printf("Palabra a eliminar: \n");
+    scanf("%s",palabra);
+    m_eliminar(map,palabra,&funcion_eliminar_claves_evaluador,&funcion_eliminar_valores_evaluador);
+
+    palabra=NULL;
+    palabra=(char*)malloc(sizeof(char));
+    printf("Palabra a eliminar: \n");
+        scanf("%s",palabra);
+    m_eliminar(map,palabra,&funcion_eliminar_claves_evaluador,&funcion_eliminar_valores_evaluador);
+
+    palabra=NULL;
+    palabra=(char*)malloc(sizeof(char));
+    printf("Palabra a eliminar: \n");
+    scanf("%s",palabra);
+    m_eliminar(map,palabra,&funcion_eliminar_claves_evaluador,&funcion_eliminar_valores_evaluador);
+*/
+    printf("destruir romper todo\n");
+    m_destruir(&map,funcion_eliminar_claves_evaluador,funcion_eliminar_valores_evaluador);
+}
+
+
